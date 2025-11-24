5058b40871d8cf28d826d989d48e4fa61bb511d1
Devon Steenberg <devon.steenberg@portainer.io>
2025-11-25 11:09:49 +1300

    chore(version): bump to v2.36.0 (#1434)

--- api/portainer.go
+++ api/portainer.go
@@ -1794,7 +1794,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.35.0"
+	APIVersion = "2.36.0"
 	// Support annotation for the API version ("STS" for Short-Term Support or "LTS" for Long-Term Support)
 	APIVersionSupport = "STS"
 	// Edition is what this edition of Portainer is called
